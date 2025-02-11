ae9d229496e1a68930c8d41215129f266ecfd4c4
nourhan <nourhansaeed707@gmail.com>
2025-02-11 05:10:03 +0200

    add swagger in product service

--- product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableWebMvc
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
--- product-service/src/main/java/com/example/product_service/config/SwaggerConfig.java
+++ product-service/src/main/java/com/example/product_service/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.user_service.Config;
+package com.example.product_service.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
